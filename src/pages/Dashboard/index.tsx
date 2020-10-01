970af3dd4bba9dbc86c84d95970494d29b451793
alexandre-roa <alexandre.z.roa@gmail.com>
2020-10-01 13:56:12 -0300

    list foods dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -33,6 +33,7 @@ interface Food {
   name: string;
   description: string;
   price: number;
+  category: number;
   thumbnail_url: string;
   formattedPrice: string;
 }
@@ -54,12 +55,17 @@ const Dashboard: React.FC = () => {
   const navigation = useNavigation();
 
   async function handleNavigate(id: number): Promise<void> {
-    // Navigate do ProductDetails page
+    return navigation.navigate('FoodDetails', { id });
   }
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // Load Foods from API
+      const response = await api.get('foods', {
+        params: {
+          category: selectedCategory,
+        },
+      });
+      setFoods(response.data);
     }
 
     loadFoods();
@@ -67,14 +73,18 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadCategories(): Promise<void> {
-      // Load categories from API
+      const response = await api.get('categories');
+
+      setCategories(response.data);
     }
 
     loadCategories();
-  }, []);
+  }, [categories]);
 
   function handleSelectCategory(id: number): void {
-    // Select / deselect category
+    const findCategory = categories.find(category => category.id === id);
+
+    findCategory && setSelectedCategory(findCategory.id);
   }
 
   return (
